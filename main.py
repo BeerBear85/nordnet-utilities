93feb26486ca8cb8a3b10fcbbcf60660ac3b8d5d
bjes <bjoern.eskildsen@man-es.com>
2023-03-30 23:46:28 +0200

    Fake pdf info + function to convert currency + get stock info

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from getpass import getpass
 #use argparse to parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--user", default="bear85", help="user name for Nordnet")
-parser.add_argument("--account", default="AI debot", help="Nordnet konto name to use")
+parser.add_argument("--account", default="AI depot", help="Nordnet konto name to use")
+parser.add_argument("--report_path", default="report.pdf", help="Path for Nordnet monthly AI report")
 args = parser.parse_args()
 
 # Get password from windows credential manager or promt user for password
@@ -16,10 +17,7 @@ if password is None:
     password = getpass("Password for " + args.user + ": ") # prompt user for password (do not show password)
 
 session = misc.login(args.user, password) #login to Nordnet
-misc.get_account_stocks(session, args.account) #get list of stocks in account
 
-stock_ticker_list = ['TSLA', 'TRYG']
-
-for stock_ticker in stock_ticker_list:
-    number = misc.get_stock_id(session, stock_ticker)
-    print(': '.join([stock_ticker, str(number)]))
+account_df = misc.get_account_stocks(session, args.account) #get list of stocks in account
+report_df = misc.read_pdf_report(args.report_path) #read the monthly pdf report from Nordnet
+diff_df = misc.generate_account_corrections(session, report_df, account_df) #generate a dataframe with the corrections to be made to the account portfolio
--- misc.py
+++ misc.py
@@ -25,33 +25,44 @@ def login(user, password):
         print('Please check that you have correctly set up nordnet_configuration.py and enabled username/password logins on Nordnet')
         exit(-1)
 
-#Get identifer number for stock from Stock Ticker Symbols
-def get_stock_id(session, stock_ticker):
+#Get info df for stock from Stock Ticker Symbols
+def get_stock_info(session, stock_ticker):
     # Set NEXT cookie and header
     url = 'https://www.nordnet.dk/markedet'
     session.get(url)
     session.headers['client-id'] = 'NEXT'
     url = 'https://www.nordnet.dk/api/2/instrument_search/query/stocklist'
-    params = {'free_text_search': stock_ticker}
+    params = {'free_text_search': stock_ticker,
+              'limit': 100}
     stock = session.get(url, params=params)
     stocks = stock.json()
     results = stocks['results']
     if len(results) == 0:
         print(f'Found no results for {stock_ticker}. Please check your search term.')
         exit(-1)
-    if len(results) > 1:
+    # check all results for exact match
+    result_list = []
+    for result in results:
+        if result['instrument_info']['symbol'] == stock_ticker:
+            result_list.append(result)
+    if len(result_list) > 1: #Don't want more than one result with the same stock ticker
         print(f'Found more than one result for {stock_ticker}. Please specify a more unique search term.')
         exit(-1)
-    number = results[0]['instrument_info']['instrument_id']
-    return number
+    result = result_list[0]
+    result_dict = {'symbol': result['instrument_info']['symbol'],
+                   'id': result['instrument_info']['instrument_id'],
+                   'name': result['instrument_info']['name'],
+                   'currency': result['instrument_info']['currency'],
+                   'price': result['price_info']['last']['price']}
+    return result_dict
 
-# Get list of stocks in account
-def get_account_stocks(session, account):
+#Get account info
+def get_account_info(session, account_alias):
     url = 'https://www.nordnet.dk/api/2/accounts'
     accounts = session.get(url)
     accounts = accounts.json()
     for acc in accounts:
-        if acc['alias'] == account:
+        if acc['alias'] == account_alias:
             account_id = acc['accno']
             break
         #if account is not found
@@ -63,14 +74,20 @@ def get_account_stocks(session, account):
     account_info_list = session.get(url)
     account_info_list = account_info_list.json()
     account_info = account_info_list[0]
+    global account_dict
     account_dict = {'market_value': account_info['full_marketvalue']['value'], #stock value
                     'account_sum': account_info['account_sum']['value'], #cash
-                    'total_value': account_info['own_capital']['value'] #total value
+                    'total_value': account_info['own_capital']['value'], #total value
+                    'currency': account_info['account_currency'], #account currency
+                    'account_id': account_id
                     }
-    account_dataframe = pd.DataFrame(account_dict, index=[0])
-    print(account_dataframe)
+    return account_dict
 
-    url = f'https://www.nordnet.dk/api/2/accounts/{account_id}/positions'
+# Get list of stocks in account
+def get_account_stocks(session, account_alias):
+    account_dict = get_account_info(session, account_alias)
+
+    url = f'https://www.nordnet.dk/api/2/accounts/{account_dict["account_id"]}/positions'
     positions = session.get(url)
     positions = positions.json()
     pos_dict_list = []
@@ -86,8 +103,57 @@ def get_account_stocks(session, account):
         pos_dict['percent'] = pos_dict['sum_price_local']/account_dict['total_value']*100
         pos_dict_list.append(pos_dict)
     pos_dataframe = pd.DataFrame(pos_dict_list)
-    print(pos_dataframe)
 
     return pos_dataframe
+
+# Method for reading the monthly pdf report from Nordnet
+def read_pdf_report(file):
+
+    # make dummy dataframe for now
+    df = pd.DataFrame(columns=['Company', 'Ticker', 'Sector', 'Weight', 'Change'])
+    #insert data into dummy dataframe
+    df.loc[0] = ['Tomra Systems', 'TOM', 'Industrials', '6%', '-1%']
+    df.loc[1] = ['Meta company', 'META', 'Tech', '5%', '1%']
+    df.loc[1] = ['Alphabet', 'GOOG', 'Tech', '8%', '-2%']
+    return df
+
+# Method for generating a dataframe with the corrections to be made to the account portfolio
+def generate_account_corrections(session, report_df, account_df):
+
+    # read the report dataframe Ticker column
+    report_tickers = report_df['Ticker'].tolist()
+
+    for ticker in report_tickers:
+        stock_info = get_stock_info(session, ticker)
+        price_in_account_currency = convert_to_account_currency(session, stock_info['price'], stock_info['currency'])
+        print(stock_info)
+    
+
+def convert_to_account_currency(session, amount, currency):
+
+    account_currency = account_dict['currency']
+    currency_convert_string = f'{currency}/{account_currency}'
+
+    # Set NEXT cookie and header
+    url = 'https://www.nordnet.dk/markedet'
+    session.get(url)
+    session.headers['client-id'] = 'NEXT'
+
+    url = 'https://www.nordnet.dk/api/2/instrument_search/query/indicator'
+    params = {'free_text_search': currency,
+              'limit': 100}
+    currency = session.get(url, params=params)
+    currency = currency.json()
+    for result in currency['results']:
+        if result['instrument_info']['name'] == currency_convert_string:
+            currency = result
+            break
+        #if currency is not found
+        if result == currency['results'][-1]: #if last currency in list
+            print(f'Currency {currency} not found. Please check your currency name.')
+            exit(-1)
+    conversion_rate = currency['price_info']['last']['price']
+    amount_in_account_currency = amount*conversion_rate
+    return amount_in_account_currency
 
 
